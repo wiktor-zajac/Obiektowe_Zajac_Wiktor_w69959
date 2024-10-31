8851210ec0c3756a1ca5822869da6bbb6d34e2c5
Wiktor Zając <w69959@student.wsiz.edu.pl>
2024-10-31 10:24:45 +0100

    Added: lab1, tasks 6 and 7

---
+++ OOP/Constructions/Construction.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP.Constructions
+{
+    internal class Construction
+    {
+        private string _buildMaterial;
+
+        public Construction(float height, float width, int entrances, int humanCapacity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+            BuildMaterial = buildMaterial;
+        }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get => _buildMaterial;
+            set => ArgumentException.ThrowIfNullOrEmpty(value);
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.Constructions;
+
+Console.WriteLine("Hello, World!");
+//var construction1 = new Construction();
+var construction2 = new Construction(23.0f, 45.0f, 4, 4, "Material");
+Console.WriteLine("Hello, World!");
+try
+{
+    var construction3 = new Construction(11, 10, 2, 2, null);
+}
+catch (ArgumentException e)
+{
+    Console.Error.WriteLine(e);
+}
